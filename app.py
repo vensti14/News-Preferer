9d47bdd61ea13b111276caf372da19dbba5774c5
vensti14 <prasadpp014@gmail.com>
2024-10-01 15:33:38 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify, request, render_template
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+NEWS_API_KEY = os.getenv('NEWS_API_KEY')
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/api/news", methods=['GET'])
+def get_news():
+    category = requests.args.get("category", "general")
+    search = requests.args.get("search", "")
+
+    url = f'https://newsapi.org/v2/top-headlines?category={category}&apiKey={NEWS_API_KEY}'
+
+    response = requests.get(url)
+    articles = response.json().get("articles", [])
+
+    if search:
+        articles = [article for article in articles if search.lower() in article ["title"].lower()]
+    
+    return jsonify({'articles': articles})
+
+if __name__ == '__main__':
+    app.run(debug =True)
+    
